1d2865a13a7ed3a88966eba8c9713592f9f42c33
nguyenthinhthanh <thanh.nguyenthinh@hcmut.edu.vn>
2026-03-24 17:20:32 +0700

    Fix: sha256sum terminated by signal 13

--- recipes-security/baseline/files/gen_baseline.sh
+++ recipes-security/baseline/files/gen_baseline.sh
@@ -10,6 +10,8 @@ TARGET_DIRS="${IMAGE_ROOTFS}/etc \
              ${IMAGE_ROOTFS}/usr/bin \
              ${IMAGE_ROOTFS}/usr/sbin"
 
+TEMP_LOG=$(mktemp)
+
 echo "# --- Statistics (Folder: File Count) ---" >> $OUTPUT
 for dir in $TARGET_DIRS; do
     if [ -d "$dir" ]; then
@@ -22,7 +24,10 @@ echo "# ---------------------------------------" >> $OUTPUT
 
 for dir in $TARGET_DIRS; do
     if [ -d "$dir" ]; then
-        find "$dir" -type f -exec sha256sum {} \; \
-        | sed "s#${IMAGE_ROOTFS}##" >> $OUTPUT
+        find "$dir" -type f -exec sha256sum {} + >> $TEMP_LOG
     fi
 done
+
+sed "s#${IMAGE_ROOTFS}##g" $TEMP_LOG >> $OUTPUT
+
+rm -f $TEMP_LOG
