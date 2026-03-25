9060317a0fe66a0110115d04f105dec821281e04
MSI\ADMINS <thanh.nguyenthinh@hcmut.edu.vn>
2026-03-25 19:43:02 +0700

    Modify file intergrity check

--- recipes-security/baseline/files/gen_baseline.sh
+++ recipes-security/baseline/files/gen_baseline.sh
@@ -8,9 +8,11 @@ mkdir -p $ROOTFS/etc/security
 rm -f $OUTPUT
 
 TARGET_DIRS="$ROOTFS/etc \
+             $ROOTFS/bin \
              $ROOTFS/sbin \
-             $ROOTFS/usr/bin \
-             $ROOTFS/usr/sbin"
+             $ROOTFS/usr/lib/python3.12/site-packages/lsmy_app \
+             $ROOTFS/usr/lib/python3.12/site-packages/lsmy_webserver \
+             $ROOTFS/usr/lib/python3.12/site-packages/lsmy_python_lib"
 
 EXCLUDE_PATTERN="etc/security/baseline.db"
 EXCLUDE_PATTERN="$EXCLUDE_PATTERN|etc/fstab"
@@ -18,7 +20,7 @@ EXCLUDE_PATTERN="$EXCLUDE_PATTERN|etc/wpa_supplicant"
 
 TEMP_LOG=$(mktemp)
 
-echo "# --- Statistics (Folder: File Count) ---" >> $OUTPUT
+echo "# --- Statistics ---" >> $OUTPUT
 for dir in $TARGET_DIRS; do
     if [ -d "$dir" ]; then
         count=$(find "$dir" -type f | grep -vE "$EXCLUDE_PATTERN" | wc -l)
