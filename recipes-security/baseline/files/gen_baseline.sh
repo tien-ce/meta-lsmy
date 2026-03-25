81c2f32a22a1defdf6d2fe7cddfecc255946833e
nguyenthinhthanh <thanh.nguyenthinh@hcmut.edu.vn>
2026-03-25 13:21:07 +0700

    Fix: exclude arg for non intergrity check

--- recipes-security/baseline/files/gen_baseline.sh
+++ recipes-security/baseline/files/gen_baseline.sh
@@ -12,16 +12,16 @@ TARGET_DIRS="$ROOTFS/etc \
              $ROOTFS/usr/bin \
              $ROOTFS/usr/sbin"
 
-EXCLUDE_ARGS="-not -path '*/etc/security/baseline.db' \
-              -not -path '*/etc/fstab' \
-              -not -path '*/etc/wpa_supplicant.conf'"
+EXCLUDE_PATTERN="etc/security/baseline.db"
+EXCLUDE_PATTERN="$EXCLUDE_PATTERN|etc/fstab"
+EXCLUDE_PATTERN="$EXCLUDE_PATTERN|etc/wpa_supplicant"
 
 TEMP_LOG=$(mktemp)
 
 echo "# --- Statistics (Folder: File Count) ---" >> $OUTPUT
 for dir in $TARGET_DIRS; do
     if [ -d "$dir" ]; then
-        count=$(find "$dir" -type f | wc -l)
+        count=$(find "$dir" -type f | grep -vE "$EXCLUDE_PATTERN" | wc -l)
         display_dir=$(echo "$dir" | sed "s#$ROOTFS##")
         echo "# $display_dir: $count files" >> $OUTPUT
     fi
@@ -30,7 +30,7 @@ echo "# ---------------------------------------" >> $OUTPUT
 
 for dir in $TARGET_DIRS; do
     if [ -d "$dir" ]; then
-        find "$dir" -type f $EXCLUDE_ARGS -exec sha256sum {} + >> $TEMP_LOG
+        find "$dir" -type f -exec sha256sum {} + | grep -vE "$EXCLUDE_PATTERN" >> $TEMP_LOG
     fi
 done
 
