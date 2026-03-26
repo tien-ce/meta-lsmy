6dfb7362fd1dad3c284d2a15aebcb3e74b8ef2dd
nguyenthinhthanh <thanh.nguyenthinh@hcmut.edu.vn>
2026-03-26 10:22:05 +0700

    Fix: gen gold backup wrong adrress

--- recipes-security/gold-backup/files/gen_gold_backup.sh
+++ recipes-security/gold-backup/files/gen_gold_backup.sh
@@ -10,12 +10,12 @@ if [ -e "$BACKUP_OUT" ]; then
     rm -f "$BACKUP_OUT"
 fi
 
-TARGET_DIRS="/etc \
-             /usr/bin \
-             /usr/sbin \
-             /usr/lib/python3.12/site-packages/lsmy_app \
-             /usr/lib/python3.12/site-packages/lsmy_webserver \
-             /usr/lib/python3.12/site-packages/lsmy_python_lib"
+TARGET_DIRS="$ROOTFS/etc \
+             $ROOTFS/usr/bin \
+             $ROOTFS/usr/sbin \
+             $ROOTFS/usr/lib/python3.12/site-packages/lsmy_app \
+             $ROOTFS/usr/lib/python3.12/site-packages/lsmy_webserver \
+             $ROOTFS/usr/lib/python3.12/site-packages/lsmy_python_lib"
 
 EXCLUDE_PATTERN="etc/security/baseline.db"
 EXCLUDE_PATTERN="$EXCLUDE_PATTERN|etc/fstab"
@@ -23,11 +23,9 @@ EXCLUDE_PATTERN="$EXCLUDE_PATTERN|etc/wpa_supplicant"
 
 FILE_LIST_TEMP=$(mktemp)
 
-for item in $TARGET_DIRS; do
-    FULL_PATH="${ROOTFS}${item}"
-    
-    if [ -e "$FULL_PATH" ]; then
-        find "$FULL_PATH" -type f | grep -vE "$EXCLUDE_PATTERN" | sed "s#$ROOTFS##" >> $FILE_LIST_TEMP
+for dir in $TARGET_DIRS; do
+    if [ -d "$dir" ]; then
+        find "$dir" -type f | grep -vE "$EXCLUDE_PATTERN" | sed "s#$ROOTFS/##g" >> $FILE_LIST_TEMP
     fi
 done
 
