d215f3067f033465d6a85d628efae0c5c4ab99a5
MSI\ADMINS <thanh.nguyenthinh@hcmut.edu.vn>
2026-04-04 14:44:26 +0700

    Fix: use echo insteadof logger

--- recipes-core/update-notify/files/check-update.sh
+++ recipes-core/update-notify/files/check-update.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-LOGTAG="lsmy-update"
-
 if ! opkg update; then
-    logger -t "$LOGTAG" "Opkg update failed"
+    echo "Opkg update failed"
     exit 1
 fi
 
 if ! UPGRADES="$(opkg list-upgradable)"; then
-    logger -t "$LOGTAG" "Opkg list-upgradable failed"
+    echo "Opkg list-upgradable failed"
     exit 1
 fi
 
-[ -n "$UPGRADES" ] && \
-logger -t "$LOGTAG" "A new version software is available, do you want to update?"
+if [ -n "$UPGRADES" ]; then
+    echo "A new version software is available, do you want to update?"
+    echo "$UPGRADES"
+fi
 
 exit 0
